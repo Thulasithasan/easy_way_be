8948a29cd7c8e7ff85aad230a557243ef3458262
Thulasithasan <65853621+Thulasithasan@users.noreply.github.com>
2025-07-20 17:42:20 +0530

    Delete target directory

--- target/classes/changelog/ddl-script/common-ddl-script-v1-add-employee-esign-role.sql
+++
@@ -1,8 +0,0 @@
--- liquibase formatted sql
-
--- changeset kalsaramagamage:common-ddl-script-v1-add-employee-esign-role
-
-ALTER TABLE `employee_role`
-    ADD COLUMN `esign_role` varchar(255) DEFAULT NULL;
-
--- rollback ALTER TABLE `employee_role` DROP COLUMN `esign_role`;
